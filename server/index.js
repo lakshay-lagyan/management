0111215642791dee74141075b8b7ff2d332a825f
Harsh <lakshaylagyan7@gmail.com>
2025-07-20 19:12:55 +0530

    api update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors    = require('cors');
-const loanRoutes = require('./server/loanRouter');
+const loanRoutes = require('./loanRouter');
 
 const app = express();
 app.use(cors());
---
+++ server/loanRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router  = express.Router();
+const loanController = require('../loanController');
+
+// Apply for a new loan
+router.post('/', loanController.applyLoan);
+
+// List all loans
+router.get('/', loanController.getLoans);
+
+module.exports = router;
